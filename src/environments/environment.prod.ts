7bec5e51f13d21dbe498f082da0ecb01b11998e5
muthonimwangi <bettymwas15@yahoo.com>
2019-05-24 18:25:02 +0300

    third commit, searchbar

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-   BASE_URL: 'http://localhost:4000'
+   //BASE_URL: 'http://dev.kladika.com/'
    //BASE_URL: 'http://ver2.kladika.com/'
+   BASE_URL: 'http://localhost:4200'
 };
