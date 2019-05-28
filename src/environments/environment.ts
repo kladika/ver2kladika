653fcadd7355dc969e0fc1bfaeb4e6f6bebac4e1
muthonimwangi <bettymwas15@yahoo.com>
2019-05-28 20:16:25 +0300

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
- //BASE_URL: 'http://dev.kladika.com/'
-  BASE_URL: 'http://localhost:4200'
+ BASE_URL: 'http://dev.kladika.com/'
+  //BASE_URL: 'http://localhost:4200'
   //BASE_URL: 'http://ver2.kladika.com/'
 };
